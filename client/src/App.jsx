8c31d8278e02cd2acfaa34d518145d6c514a1d34
adya-r <adyaraut123@gmail.com>
2024-08-05 20:12:31 +0530

    update all routes

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import {Route,Routes} from "react-router-dom";
+import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from './pages/LoginPage.jsx';
+import Layout from './Layout.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import axios from 'axios';
+import { UserContextProvider } from './UserContext.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import PlacesPage from './pages/PlacesPage.jsx';
+import PlacesFormPage from './pages/PlacesFormPage.jsx';
+import BookingsPage from './pages/BookingsPage.jsx';
+import SingleBookingPage from './pages/SingleBookingPage.jsx';
+import SinglePlacePage from './pages/SinglePlacePage.jsx';
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials=true;
+
+function App() {
+  
+    
+  return (
+    <UserContextProvider>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route  index element={<IndexPage/>}/>
+      <Route  path="/login" element={<LoginPage/>}/>
+      <Route  path="/register" element={<RegisterPage/>}/>
+      <Route path="/account" element={<ProfilePage/>} />
+      <Route path="/account/places" element={<PlacesPage/>} />
+      <Route path="/account/places/new" element={<PlacesFormPage/>} />
+      <Route path="/account/places/:id" element={<SinglePlacePage/>} />
+      <Route path="/places/:id" element={<SinglePlacePage/>} />
+      <Route path="/account/bookings" element={<BookingsPage/>} />
+      <Route path="/account/bookings/:id" element={<SingleBookingPage/>} />
+      </Route>
+      
+      
+    </Routes>
+    </UserContextProvider>
+
+  )
+}  
+
+export default App

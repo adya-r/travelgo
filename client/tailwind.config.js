4089de0dfb26a18c3ab6073cc39db748df606683
adya-r <adyaraut123@gmail.com>
2024-08-05 20:03:27 +0530

    update packages

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#0b0c60',
+      },
+    },
+  },
+  plugins: [],
+}
+
